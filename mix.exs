43660e5f25fbe3052fb7873929a63b1a6f027394
okothkongo1 <okothkongo@gmail.com>
2019-05-16 11:51:49 +0300

    setup credo

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule ReTither.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_integration, "~> 0.6", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
